3a2d8889c75938f2cf8711f49aae59bc33ece616
mingcaocao <mingcaocao@hotmail.com>
2018-02-22 23:38:03 -0500

    Big table added.

--- service/main.go
+++ service/main.go
@@ -13,14 +13,15 @@ import (
 	"context"
 	"cloud.google.com/go/storage"
 	"io"
+	"cloud.google.com/go/bigtable"
 )
 
 const(
 	DISTANCE = "200km"
 	INDEX = "around"
 	TYPE = "post"
-	//PROJECT_ID = "maximal-zoo-194922"
-	//BT_INSTANCE = ""
+	PROJECT_ID = "maximal-zoo-194922"
+	BT_INSTANCE = "around-post"
 	ES_URL      = "http://35.227.69.222:9200"
 	BUCKET_NAME = "post-images-maximal-zoo-194922"
 )
@@ -130,6 +131,28 @@ func handlerPost(w http.ResponseWriter, r * http.Request) {
 
 	// Save to BigTable.
 	//saveToBigTable(p, id)
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 
 }
 
